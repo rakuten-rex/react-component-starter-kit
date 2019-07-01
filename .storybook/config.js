0a45af03d460f2f8c96e69ab82a3e704483973fd
Claudio <claudio.moya@rakuten.com>
2019-07-01 14:03:11 +0900

    Added - ReX theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import rexTheme from './rexTheme';
+
+// Option defaults.
+addParameters({
+  options: {
+    theme: rexTheme,
+  },
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
