14a4d63fed69b472076b3fd27622b6c4a4f41fd8
Claudio <claudio.moya@rakuten.com>
2019-07-03 14:16:07 +0900

    Removed - Temporary backups

--- _backup/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@rakuten-rex/eslint-config-rex-react-component', 'plugin:prettier/recommended'],
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-  },
-};
--- _backup/.storybook/addons.js
+++
@@ -1,6 +0,0 @@
-import '@storybook/addon-knobs/register';
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-actions/register';
-import '@storybook/addon-viewport/register';
-import '@storybook/addon-google-analytics/register';
-window.STORYBOOK_GA_ID = 'UA-139696530-2';
