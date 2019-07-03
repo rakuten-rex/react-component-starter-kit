2efa45677c62e42fa0571ea3524e13050b5f97c0
Claudio <claudio.moya@rakuten.com>
2019-07-03 10:51:32 +0900

    Fixed - Info related to package and some eslint

--- init.js
+++ init.js
@@ -39,7 +39,7 @@ rl.question('\nPackage name (ex: text): ', packageName => {
 
       console.log(`./webpack.config.js: \n
         const entry = {};
-        entry[package.name] = './src/${componentName}.jsx';
+        entry[packageNameOnly] = './src/${componentName}.jsx';
       `);
 
       rl.question('Are you sure you want to proceed? (y/n) ', answer => {
@@ -128,7 +128,10 @@ function setPackageJson(packageName, componentName, packageJSON) {
     starterKitScope,
     scopePackageName
   );
-  packageData.author = packageData.author.replace(starterKitScope, scopePackageName);
+  packageData.author = packageData.author.replace(
+    starterKitScope,
+    scopePackageName
+  );
   packageData.bugs.url = packageData.bugs.url.replace(
     starterKitScope,
     scopePackageName
