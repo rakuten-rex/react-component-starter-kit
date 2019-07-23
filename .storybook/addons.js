4f11c52845de4c8d1f6ca1db6c921d1a90e3bc31
Claudio <claudio.moya@rakuten.com>
2019-07-23 14:47:49 +0900

    Added - Custom add-on to set a dynamic page title for tracking

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -4,4 +4,5 @@ import '@storybook/addon-actions/register';
 import '@storybook/addon-viewport/register';
 import '@rakuten-rex/storybook-inspecthtml/register';
 import '@rakuten-rex/storybook-inspectjsx/register';
+import './addon-page-title/register';
 import './addon-google-analytics/register';
