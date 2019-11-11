7dd405df952146d70d9db0c1e645e02ee4a469dd
Claudio <claudio.moya@rakuten.com>
2019-11-11 14:18:52 +0900

    Added - Theme using ReX assets

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import rexTheme from './theme/rexTheme';
+
+addParameters({
+  options: {
+    theme: rexTheme,
+  },
+});
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src', true, /\.stories\.jsx$/), module);
