69841c77cdc3133c7c1110c83865da58536497b7
Claudio <claudio.moya@rakuten.com>
2019-11-11 14:50:30 +0900

    Added - Story Source addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,4 +3,5 @@ import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import '@rakuten-rex/storybook-inspecthtml/register';
 import '@rakuten-rex/storybook-inspectjsx/register';
+import '@storybook/addon-storysource/register';
 import '@storybook/addon-a11y/register';
