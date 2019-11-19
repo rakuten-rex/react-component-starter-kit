946d7a3d05ace5b67a858741a64033091c266c84
Claudio <claudio.moya@rakuten.com>
2019-11-19 14:32:58 +0900

    Added - Base setup for storybook

---
+++ docs/main.2e98e8179daffe228611.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(370),c(482),n.exports=c(771)},1:function(n,o){},482:function(n,o,c){"use strict";c.r(o);c(483),c(768)}},[[0,1,2]]]);
