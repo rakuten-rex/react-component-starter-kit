a9532ff71aebb41234d41dec5daeb6855427e680
Claudio <claudio.moya@rakuten.com>
2019-11-21 14:57:56 +0900

    Removed - Old storybook folders

--- .storybook/addons.js
+++
@@ -1,8 +0,0 @@
-import '@storybook/addon-knobs/register';
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-actions/register';
-import '@storybook/addon-viewport/register';
-import '@rakuten-rex/storybook-inspecthtml/register';
-import '@rakuten-rex/storybook-inspectjsx/register';
-import './addon-page-title/register';
-import './addon-google-analytics/register';
