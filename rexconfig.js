0396e15d9f90b3fc27d173ebf0464ce6cbbab03b
Claudio <claudio.moya@rakuten.com>
2019-11-27 17:23:02 +0900

    Added - Automatization for rexconfig entry components for webpack

--- rexconfig.js
+++ rexconfig.js
@@ -1,20 +1,52 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /**
  * ReX React Components Starter kit
- * Project entry points for webpack
+ * Project entry points for webpack (automated version)
  */
+const fs = require('fs');
+const glob = require('glob');
 
 // Entry points for React components
-const entry = {
-  'MyComponent/MyComponent': './src/MyComponent/MyComponent.jsx',
-};
+const componentList = glob.sync('./src/*/index.jsx');
+
+const entry = componentList.reduce((obj, item) => {
+  const name = item.replace('./src/', '').replace('/index.jsx', '');
+  const path = `${name}/${name}`;
+  const component = {};
+  component[path] = `./src/${path}.jsx`;
+
+  return Object.assign(obj, component);
+}, {});
 
 // Entry points for NPM distribution assets
+const index = fs
+  .readFileSync('src/index.jsx')
+  .toString()
+  .trim()
+  .replace("export { default } from './", '')
+  .replace("';", '');
+
+const components = componentList.map(item =>
+  item.replace('./src/', '').replace('/index.jsx', '')
+);
+
 const npmFiles = {
-  index: 'MyComponent',
-  components: ['MyComponent'],
+  index,
+  components,
 };
 
-// Export to webpack.config.js
+/**
+ * Export to webpack.config.js
+ * Example:
+ *  const entry = {
+ *     'MyComponent/MyComponent': './src/MyComponent/MyComponent.jsx',
+ *   };
+ *
+ *  const npmFiles = {
+ *    index: 'MyComponent',
+ *    components: ['MyComponent'],
+ *  };
+ */
 module.exports = {
   entry,
   npmFiles,
