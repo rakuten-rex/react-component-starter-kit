2af34d5fa68e346bdf92314b19d522e3c7cc6c07
Claudio <claudio.moya@rakuten.com>
2019-11-27 12:15:56 +0900

    Update - Rex Component template creation and added isDefaultExport option

--- project-scripts/plop-templates/RexComponent/plop-generator.js
+++ project-scripts/plop-templates/RexComponent/plop-generator.js
@@ -1,68 +1,66 @@
 module.exports = plop => {
   plop.setGenerator('rex-component', {
     description: 'Create a ReX React Component',
-    // User input prompts provided as arguments to the template
     prompts: [
       {
-        // Raw text input
         type: 'input',
-        // Variable name for this input
         name: 'name',
-        // Prompt to display on command line
         message: 'What is your component name?',
       },
-    ],
-    actions: [
       {
-        // Add a new file
-        type: 'add',
-        // Path for the new file
-        path: '../../../src/{{pascalCase name}}/index.jsx',
-        // Handlebars template used to generate content of new file
-        templateFile: 'index.jsx.hbs',
-      },
-      {
-        // Add a new file
-        type: 'add',
-        // Path for the new file
-        path: '../../../src/{{pascalCase name}}/{{pascalCase name}}.jsx',
-        // Handlebars template used to generate content of new file
-        templateFile: 'MyComponent.jsx.hbs',
-      },
-      {
-        // Add a new file
-        type: 'add',
-        // Path for the new file
-        path: '../../../src/{{pascalCase name}}/{{pascalCase name}}.scss',
-        // Handlebars template used to generate content of new file
-        templateFile: 'MyComponent.scss.hbs',
-      },
-      {
-        // Add a new file
-        type: 'add',
-        // Path for the new file
-        path:
-          '../../../src/{{pascalCase name}}/_{{pascalCase name}}.mixin.scss',
-        // Handlebars template used to generate content of new file
-        templateFile: '_MyComponent.mixin.scss.hbs',
-      },
-      {
-        // Add a new file
-        type: 'add',
-        // Path for the new file
-        path:
-          '../../../src/{{pascalCase name}}/{{pascalCase name}}.stories.jsx',
-        // Handlebars template used to generate content of new file
-        templateFile: 'MyComponent.stories.jsx.hbs',
-      },
-      {
-        // Add a new file
-        type: 'add',
-        // Path for the new file
-        path: '../../../src/{{pascalCase name}}/_core.mixin.scss',
-        // Handlebars template used to generate content of new file
-        templateFile: '_core.mixin.scss',
+        type: 'confirm',
+        name: 'isDefaultExport',
+        message: 'Is default?',
       },
     ],
+    actions(data) {
+      const { isDefaultExport } = data;
+
+      const actionsList = [
+        {
+          type: 'add',
+          path: '../../../src/{{pascalCase name}}/index.jsx',
+          templateFile: 'index.jsx.hbs',
+        },
+        {
+          type: 'add',
+          path: '../../../src/{{pascalCase name}}/{{pascalCase name}}.jsx',
+          templateFile: 'MyComponent.jsx.hbs',
+        },
+        {
+          type: 'add',
+          path: '../../../src/{{pascalCase name}}/{{pascalCase name}}.scss',
+          templateFile: 'MyComponent.scss.hbs',
+        },
+        {
+          type: 'add',
+          path:
+            '../../../src/{{pascalCase name}}/_{{pascalCase name}}.mixin.scss',
+          templateFile: '_MyComponent.mixin.scss.hbs',
+        },
+        {
+          type: 'add',
+          path:
+            '../../../src/{{pascalCase name}}/{{pascalCase name}}.stories.jsx',
+          templateFile: 'MyComponent.stories.jsx.hbs',
+        },
+        {
+          type: 'add',
+          path: '../../../src/{{pascalCase name}}/_core.mixin.scss',
+          templateFile: '_core.mixin.scss',
+        },
+      ];
+
+      if (isDefaultExport) {
+        actionsList.push({
+          type: 'add',
+          path: '../../../src/index.jsx',
+          template: "export { default } from './{{pascalCase name}}';\n",
+          force: true,
+        });
+      }
+
+      return actionsList;
+    },
   });
 };
--- project-scripts/plop-templates/package-json/plop-generator.js
+++ project-scripts/plop-templates/package-json/plop-generator.js
@@ -1,31 +1,27 @@
 const packageJson = require('../../../package.json');
 
-const template = JSON.stringify(packageJson, null, 2).replace(
-  new RegExp('react-component-starter-kit', 'g'),
+const { name } = packageJson;
+const componentName = name.replace('@rakuten-rex/', '');
+
+const template = `${JSON.stringify(packageJson, null, 2)}\n`.replace(
+  new RegExp(componentName, 'g'),
   '{{dashCase name}}'
 );
 
 module.exports = plop => {
   plop.setGenerator('package-json', {
     description: 'Update package.json with custom repository settings',
-    // User input prompts provided as arguments to the template
     prompts: [
       {
-        // Raw text input
         type: 'input',
-        // Variable name for this input
         name: 'name',
-        // Prompt to display on command line
         message: 'What is your package name?',
       },
     ],
     actions: [
       {
-        // Add a new file
         type: 'add',
-        // Path for the new file
         path: '../../../package.json',
-        // Handlebars template used to generate content of new file
         template,
         force: true,
       },
