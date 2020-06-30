802f105ce2ce2b189092287e74dfdeb42aab65b8
Claudio <claudio.moya@rakuten.com>
2020-06-30 17:27:21 +0900

    Added - Docs for github pages and README file

--- docs/v/latest/MyComponent/MyComponent.development.js
+++ docs/v/latest/MyComponent/MyComponent.development.js
@@ -1,6 +1,6 @@
 /*!
  * 
- * @license @rakuten-rex/react-component-starter-kit v3.5.0 2020-06-30
+ * @license @rakuten-rex/react-component-starter-kit v3.6.0 2020-06-30
  * MyComponent/MyComponent.development.js
  * 
  * Copyright (c) 2018-present, Rakuten, Inc.
